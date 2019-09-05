846de57f929cf75c457acfd7b770ccadceb61eb8
yar0316_note <yaworkon@gmail.com>
2019-09-05 16:17:39 +0900

    2章完走

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
+  root 'users#index'
   get 'helloworld/hi'
   get 'hello/hello'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
